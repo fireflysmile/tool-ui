817636681fd98236090ce7080cf5fb7b11b008ee
dinhsinh <duongdinhsinh@gmail.com>
2019-03-26 02:22:51 +0700

    create p-dropdown, datepicker

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -7,7 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceComponent implements OnInit {
 
-  constructor() { }
+  computerList = [];
+  computer = {};
+
+  departmentList = [];
+  department = {};
+
+  locationList = [];
+  location = {};
+
+  mirgationDate = null;
+  roCalender: any;
+
+  constructor() {
+    this.roCalender = {
+      firstDayOfWeek: 1,
+      dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+      monthNames: [ 'January', 'February', 'March', 'April',
+      'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+      monthNamesShort: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+      today: 'Today',
+      clear: 'Clear',
+      dateFormat: 'mm/dd/yy'
+  };
+    this.computerList = [{computer_name: 'hp'}, {computer_name: 'asus'}, {computer_name: 'macbook'}];
+    this.computer = {computer_name: 'hp'};
+
+    this.departmentList = [{department_name: 'hp'}, {department_name: 'asus'}, {department_name: 'macbook'}];
+    this.department = {department_name: 'hp'};
+
+    this.locationList = [{office_location: 'hp'}, {office_location: 'asus'}, {office_location: 'macbook'}];
+    this.location = {office_location: 'hp'};
+  }
 
   ngOnInit() {
   }
