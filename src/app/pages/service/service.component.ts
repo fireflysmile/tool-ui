726a7058caeb3bd772e4d23d6498ea18b4db3e26
fireflysmile <fireflysmile@users.noreply.github.com>
2019-03-29 00:17:44 +0700

    Revert "Update/update service page css"

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -18,16 +18,6 @@ export class ServiceComponent implements OnInit {
   location = '';
   mirgationDate = null;
   roCalender: any;
-  
-  checkList = [
-  {label: 'Yes', value: true},
-  {label: 'No', value: false}
-];
- appList = [
-    {id: 'app1', name: 'Application 1', value: true},
-    {id: 'app2', name: 'Application 2', value: true},
-    {id: 'app3', name: 'Application 3', value: true}
-  ];
 
   datePlan: any;
 
@@ -64,9 +54,4 @@ export class ServiceComponent implements OnInit {
     );
   }
 
-  findDate(date= {day: null, month: null, year: null}) {
-    return !!this.datePlanList.find((curDate, i) => {
-      return curDate.year === date.year && curDate.month === date.month && curDate.day === date.day;
-    });
-  }
 }
